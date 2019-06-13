44bf8a4903d7d1eb0707066faea45f1b491b11ee
rezaiymohammadali <rezaiymohammadali@ymail.com>
2019-06-13 16:37:44 +0430

    Add a log to project

--- app/src/main/java/com/mar/myapplication/MainActivity.java
+++ app/src/main/java/com/mar/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.mar.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this,"Hello World!!",Toast.LENGTH_LONG).show();
+
+        Log.e("aaa","This Log is for test ...");
 
     }
 }
